b5ee7622ffb23a1ef127d8824b379b57793f3320
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-12-25 20:18:57 +0000

    temp get and delete user done

--- account-service/internal/repo.go
+++ account-service/internal/repo.go
@@ -23,8 +23,9 @@ var sess = session.Must(session.NewSessionWithOptions(session.Options{
 	SharedConfigState: session.SharedConfigEnable,
 }))
 
+svc := dynamodb.New(sess)
+
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	svc := dynamodb.New(sess)
 	body, err := ioutil.ReadAll(r.Body)
 
 	// Create User using body info being passed in when the endpoint is called
@@ -64,7 +65,38 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("")
+
+	var u internal.User
+
+    // Try to decode the request body into the struct. If there is an error,
+    // respond to the client with the error message and a 400 status code.
+    err := json.NewDecoder(r.Body).Decode(&u)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	user_id := u.uuid
+
+	input := &dynamodb.DeleteItemInput{
+    	Key: map[string]*dynamodb.AttributeValue{
+        	"uuid": {
+            	N: aws.String(movieYear),
+        	},
+    	},
+		TableName: aws.String("Users"),
+	}
+
+	_, err := svc.DeleteItem(input)
+
+	if err != nil {
+		w.WriteHeader(http.StatusFailedDependency)
+		w.Write([]byte("424 - DynamoDB DeleteItem Failed"))
+		return
+	}
+	
+	fmt.Println("Deleted User from table.")
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +104,50 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("")
+
+	var u internal.User
+
+    // Try to decode the request body into the struct. If there is an error,
+    // respond to the client with the error message and a 400 status code.
+    err := json.NewDecoder(r.Body).Decode(&u)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+    	TableName: aws.String("Users"),
+    	Key: map[string]*dynamodb.AttributeValue{
+        	"uuid": {
+            	N: aws.String(u.uuid),
+        	},
+    	},
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+   		fmt.Println(err.Error())
+    	return
+	}
+
+	item := Item{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+
+	if err != nil {
+    	panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	if item.Title == "" {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write([]byte("204 - Not user found"))
+    	fmt.Println("Could not find user")
+    	return
+	}
+
+	fmt.Println("User found: ", item)
+	w.WriteHeader(http.StatusOK)
+	return item
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
