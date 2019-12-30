f50c008f688c7601133b80dbf9c00e7e6587f1d1
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-12-30 11:30:12 +0000

    changes

--- account-service/internal/routes.go
+++ account-service/internal/routes.go
@@ -5,22 +5,37 @@ import (
 	"github/Get-me-in/account-service/configs"
 	"log"
 	"net/http"
+	"github/Get-me-in/pkg/security"
+	"fmt"
 )
+
+func wrapHandlerWithaAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		a := req.Header.Get("Authorization")
+
+		fmt.Println(a,"asd")
+		if a != "" && security.VerifyToken(a) {
+			handler(w,req)
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
 
 func SetupEndpoints() {
 
 	_router := mux.NewRouter()
 
-	_router.HandleFunc("/test", TestFunc)
+	_router.HandleFunc("/test", wrapHandlerWithaAuth(TestFunc))
 	_router.HandleFunc("/connect", ConnectToInstance)
 
 	// Register
-	_router.HandleFunc("/account", CreateUser).Methods("PUT")
-	_router.HandleFunc("/account", DeleteUser).Methods("DELETE")
-	_router.HandleFunc("/account", UpdateUser).Methods("PATCH")
-	_router.HandleFunc("/account", GetUser).Methods("GET")
+	_router.HandleFunc("/account", wrapHandlerWithaAuth(CreateUser)).Methods("PUT")
+	_router.HandleFunc("/account", wrapHandlerWithaAuth(DeleteUser)).Methods("DELETE")
+	_router.HandleFunc("/account", wrapHandlerWithaAuth(UpdateUser)).Methods("PATCH")
+	_router.HandleFunc("/account", wrapHandlerWithaAuth(GetUser)).Methods("GET")
 	//_router.HandleFunc("/account", GetUsers).Methods("GET")
-	_router.HandleFunc("/account/verify", Login).Methods("POST")
+	_router.HandleFunc("/account/verify", wrapHandlerWithaAuth(Login)).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(configs.PORT, _router))
 }
