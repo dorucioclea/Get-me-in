07bd4696c105c9a1ea76b9271a40452a149c5bc4
SweetTreats <sumite3117@hotmail.com>
2020-01-26 16:25:01 +0000

    refactor to handle errors

--- pkg/dynamodb/get_item.go
+++ pkg/dynamodb/get_item.go
@@ -3,15 +3,16 @@ package dynamodb
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"net/http"
 )
 
-func GetItem(identifier string) (*dynamodb.GetItemOutput, error) {
+func GetItem(itemValue string) (*dynamodb.GetItemOutput, error) {
 
 	result, err := DynamoConnection.GetItem(&dynamodb.GetItemInput{
 		TableName: aws.String(DynamoTable),
 		Key: map[string]*dynamodb.AttributeValue{
 			SearchParam: {
-				S: aws.String(identifier),
+				S: aws.String(itemValue),
 			},
 		},
 	})
@@ -21,7 +22,10 @@ func GetItem(identifier string) (*dynamodb.GetItemOutput, error) {
 	}
 
 	if result.Item == nil {
-		return nil, CustomerError("Not Found")
+		return nil, &ErrorString{
+			Reason: http.StatusText(http.StatusNotFound),
+			Code:   http.StatusNotFound,
+		}
 	}
 
 	return result, nil
