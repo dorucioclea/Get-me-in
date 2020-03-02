d6dc939f973fe7de2cf4b2657eedc55fb930445b
SweetTreats <sumite3117@hotmail.com>
2020-03-02 20:16:27 +0000

    minor refactor

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/ProjectReferral/Get-me-in/auth-service/configs"
 	"github.com/ProjectReferral/Get-me-in/auth-service/internal"
-	"net/http"
 )
 
 func main() {
 	internal.SetupEndpoints()
-	http.ListenAndServe(configs.PORT, nil)
 }
