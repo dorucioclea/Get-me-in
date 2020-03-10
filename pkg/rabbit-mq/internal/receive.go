d2378d0934b67d6b9799816f9021b9b567309e80
SweetTreats <sumite3117@hotmail.com>
2020-03-10 17:16:05 +0000

    minor changes

--- pkg/rabbit-mq/internal/receive.go
+++ pkg/rabbit-mq/internal/receive.go
@@ -8,6 +8,7 @@ import (
 
 func ReceiveFromQ(qName string){
 	conn, err := amqp.Dial(configs.BrokerUrl)
+	log.Printf("Listening on Q: %s", qName)
 
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -16,20 +17,12 @@ func ReceiveFromQ(qName string){
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		qName, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
 	failOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
-		q.Name, // queue
+		qName, // queue
 		"",     // consumer
-		true,   // auto-ack
+		true,   // auto-ack, TODO: manual ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
@@ -37,7 +30,7 @@ func ReceiveFromQ(qName string){
 	)
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
+	forever := make(chan string)
 
 	go func() {
 		for d := range msgs {
@@ -45,9 +38,9 @@ func ReceiveFromQ(qName string){
 		}
 	}()
 
+	<-forever
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
 
 func failOnError(err error, msg string) {
